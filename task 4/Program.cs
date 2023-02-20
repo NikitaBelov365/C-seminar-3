abe2ee786cded7136b58ab92e949b5710baaf8cd
Nikitabelov365 <nikitabelov365@gmail.com>
2023-02-20 11:13:58 +0300

    task 5 solved

---
+++ task 4/Program.cs
@@ -0,0 +1,19 @@
+﻿int InputInt(string message)
+{
+    System.Console.WriteLine($"{message}>");
+    int value;
+    if (int.TryParse(Console.ReadLine(), out value))
+    {
+        return value;
+    }
+    System.Console.WriteLine("Its not a number");
+    Environment.Exit(1);
+    return 0;
+}
+int number = InputInt("Specify the number of restrictions for calculating squares from 1");
+
+for (int i = 1; i <= number; i++) // int i - инициализация, второе условие выполнения, третье - итерирование
+{
+    int square = i*i;
+    System.Console.WriteLine($"{square}");
+}
